0f0de45e953d5f800b07538a0375687d8c702830
SheemaBuilder <sheemaparwaz@gmail.com>
2024-12-19 21:57:30 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
+import BuilderDevTools from "@builder.io/dev-tools/next";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig = BuilderDevTools()({
   /* config options here */
   reactStrictMode: true,
-};
+});
 
 export default nextConfig;
